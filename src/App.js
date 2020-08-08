a3024994f5eacd01d58e77593d082c6fdb9a837c
Seim2k17 <sng198022@googlemail.com>
2020-08-08 11:31:49 +0200

    fg

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React.js
+            Learn React.js 2000
           </a>
         </header>
       </div>
